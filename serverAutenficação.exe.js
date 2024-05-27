440a9dc792ccb9e0d5a7dd955432f5984f53b05e
zelita-lima <163172165+zelita-lima@users.noreply.github.com>
2024-05-27 17:40:46 -0300

    Update
    27/05

--- serverAutenficação.exe.js
+++ serverAutenficação.exe.js
@@ -81,14 +81,15 @@ app.post('/Frutas/Inserir', authenticate, (req, res) => {
 app.put('/Frutas/Atualizar/:id', authenticate, (req, res) => {
   const id = parseInt(req.params.id); // Converte o parâmetro ID para um número inteiro
   console.log(`Recebida requisição PUT para ID: ${id} com dados: `, req.body); // Loga o ID e os dados recebidos
-  const index = Frutas.findIndex(s => s.id === id); // Encontra o índice do servidor com o ID especificado
+  const index = servidores.findIndex(s => s.id === id); // Encontra o índice do servidor com o ID especificado
   if (index !== -1) {
     Frutas[index] = { ...Frutas[index], ...req.body }; // Atualiza o servidor com os novos dados
     res.json(Frutas[index]); // Responde com o servidor atualizado
   } else {
-    res.status(404).send('Frutas não encontradas'); // Responde com status 404 se o servidor não for encontrado
+    res.status(404).send('Servidor não encontrado'); // Responde com status 404 se o servidor não for encontrado
   }
 });
+
 
 // Rota para atualizar parcialmente um servidor existente (protegida por autenticação)
 app.patch('/Frutas/Atualizar/:id', authenticate, (req, res) => {
